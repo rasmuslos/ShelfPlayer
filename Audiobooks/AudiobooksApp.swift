7091850220be38729a01c3d33f49c38207c04743
Rasmus Krämer <git@rfk.io>
2023-09-16 19:57:10 +0200

    Initial Commit

---
+++ Audiobooks/AudiobooksApp.swift
@@ -0,0 +1,32 @@
+//
+//  AudiobooksApp.swift
+//  Audiobooks
+//
+//  Created by Rasmus Krämer on 16.09.23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AudiobooksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
