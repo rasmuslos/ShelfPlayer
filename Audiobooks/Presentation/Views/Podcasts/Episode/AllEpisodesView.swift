23c26315628af4fbaf8d5e3b10b2b224863c5a3d
Rasmus Krämer <git@rfk.io>
2023-10-14 15:17:12 +0200

    Added episode search

--- Audiobooks/Presentation/Views/Podcasts/Episode/AllEpisodesView.swift
+++ Audiobooks/Presentation/Views/Podcasts/Episode/AllEpisodesView.swift
@@ -11,6 +11,8 @@ struct AllEpisodesView: View {
     let episodes: [Episode]
     let podcastId: String
     
+    @State var query = ""
+    
     @State var filter: EpisodeFilterSortMenu.Filter
     @State var sortOrder: EpisodeFilterSortMenu.SortOrder
     @State var ascending: Bool
@@ -26,10 +28,14 @@ struct AllEpisodesView: View {
     
     var body: some View {
         List {
-            EpisodesList(episodes: EpisodeFilterSortMenu.filterAndSortEpisodes(episodes, filter: filter, sortOrder: sortOrder, ascending: ascending))
+            EpisodesList(episodes: EpisodeFilterSortMenu.filterAndSortEpisodes(episodes, filter: filter, sortOrder: sortOrder, ascending: ascending).filter {
+                let query = query.lowercased()
+                return query == "" || $0.sortName.contains(query) || $0.name.lowercased().contains(query) || ($0.descriptionText?.lowercased() ?? "").contains(query)
+            })
         }
         .listStyle(.plain)
         .navigationTitle("Episodes")
+        .searchable(text: $query)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 EpisodeFilterSortMenu(podcastId: podcastId, filter: $filter, sortOrder: $sortOrder, ascending: $ascending)
