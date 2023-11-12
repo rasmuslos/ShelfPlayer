c31214417ab9dc7313a15aa9e14db26304d99736
Rasmus Krämer <git@rfk.io>
2023-11-12 21:35:26 +0100

    Multiplatform proof of concept

---
+++ tvOS/Core/tvOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  tvOS.swift
+//  tvOS
+//
+//  Created by Rasmus Krämer on 12.11.23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct tvOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
