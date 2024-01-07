f7c83f090eb26a56c407250073dda32f71dafb49
Rasmus Krämer <git@rfk.io>
2024-01-07 21:21:10 +0100

    Added authors view

---
+++ iOS/Components/Audiobooks/Author/AuthorListRow.swift
@@ -0,0 +1,29 @@
+//
+//  AuthorListRow.swift
+//  iOS
+//
+//  Created by Rasmus Krämer on 07.01.24.
+//
+
+import SwiftUI
+import ShelfPlayerKit
+
+struct AuthorListRow: View {
+    let author: Author
+    
+    var body: some View {
+        HStack {
+            ItemImage(image: author.image)
+                .frame(width: 60)
+                .clipShape(RoundedRectangle(cornerRadius: 10000))
+            
+            Text(author.name)
+        }
+    }
+}
+
+#Preview {
+    List {
+        AuthorListRow(author: Author.fixture)
+    }
+}
---
+++ iOS/Views/Audiobooks/Author/AuthorsView.swift
@@ -0,0 +1,53 @@
+//
+//  AuthorsView.swift
+//  iOS
+//
+//  Created by Rasmus Krämer on 07.01.24.
+//
+
+import SwiftUI
+import ShelfPlayerKit
+
+struct AuthorsView: View {
+    @Environment(\.libraryId) var libraryId
+    
+    @State var authors: [Author]?
+    @State var failed = false
+    
+    var body: some View {
+        if let authors, !authors.isEmpty {
+            List {
+                ForEach(authors) { author in
+                    NavigationLink(destination: AuthorView(author: author)) {
+                        AuthorListRow(author: author)
+                    }
+                }
+            }
+            .navigationTitle("authors.title")
+        } else if failed {
+            ErrorView()
+        } else {
+            LoadingView()
+                .onAppear(perform: loadAuthors)
+        }
+    }
+}
+
+extension AuthorsView {
+    @Sendable
+    func loadAuthors() {
+        failed = false
+        
+        Task {
+            do {
+                authors = try await AudiobookshelfClient.shared.getAuthors(libraryId: libraryId)
+            } catch {
+                failed = true
+            }
+        }
+    }
+}
+
+#Preview {
+    AuthorsView()
+}
