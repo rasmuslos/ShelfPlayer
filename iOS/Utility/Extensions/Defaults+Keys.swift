b432855c9e16e187e6cba0ee55f7c89cf0356883
Rasmus Krämer <git@rfk.io>
2024-02-17 11:46:48 +0100

    Cloes #48

--- iOS/Utility/Extensions/Defaults+Keys.swift
+++ iOS/Utility/Extensions/Defaults+Keys.swift
@@ -9,6 +9,8 @@ import Foundation
 import Defaults
 
 extension Defaults.Keys {
+    static let customSleepTimer = Key<Int>("customSleepTimer", default: 0)
+    
     static let authorsAscending = Key<Bool>("authorsAscending", default: true)
     static let showAuthorsRow = Key<Bool>("showAuthorsRow", default: false)
     static let disableDiscoverRow = Key<Bool>("disableDiscoverRow", default: false)
