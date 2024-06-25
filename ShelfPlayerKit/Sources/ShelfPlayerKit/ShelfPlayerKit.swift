0f6139078261828f48175db6a6f73b2450297f05
Rasmus Krämer <git@rfk.io>
2024-06-25 11:23:34 +0200

    Update SPFoundation

---
+++ ShelfPlayerKit/Sources/ShelfPlayerKit/ShelfPlayerKit.swift
@@ -0,0 +1,16 @@
+//
+//  ShelfPlayerKit.swift
+//  
+//
+//  Created by Rasmus Krämer on 22.06.24.
+//
+
+@_exported import SPFoundation
+@_exported import SPExtension
+
+@_exported import SPNetwork
+@_exported import SPOffline
+
+#if canImport(SPOfflineExtended)
+@_exported import SPOfflineExtended
+#endif
