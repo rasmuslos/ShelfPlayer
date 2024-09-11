379b1b444d93ba3576d8a7ef863f0c59cc5764e3
Rasmus Krämer <git@rfk.io>
2024-09-11 13:33:02 +0200

    Reimplement sleep timer

---
+++ ShelfPlayerKit/Sources/SPPlayback/Extensions/DispatchTimeInterval+TimeInterval.swift
@@ -0,0 +1,25 @@
+//
+//  DispatchTimeInterval+TimeInterval.swift
+//  ShelfPlayerKit
+//
+//  Created by Rasmus Krämer on 11.09.24.
+//
+
+import Foundation
+
+public extension DispatchTimeInterval {
+    var timeInterval: TimeInterval? {
+        switch self {
+            case .seconds(let value):
+                TimeInterval(value)
+            case .milliseconds(let value):
+                TimeInterval(value) * 0.001
+            case .microseconds(let value):
+                TimeInterval(value) * 0.000_001
+            case .nanoseconds(let value):
+                TimeInterval(value) * 0.000_000_001
+            default:
+                nil
+        }
+    }
+}
