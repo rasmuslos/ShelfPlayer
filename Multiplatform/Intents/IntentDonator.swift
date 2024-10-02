19ac711d2887ae6db6da9d4bbaf2f686f7111895
Rasmus Krämer <git@rfk.io>
2024-10-02 08:16:53 +0200

    Fix resume playback (Closes #143)

--- Multiplatform/Intents/IntentDonator.swift
+++ Multiplatform/Intents/IntentDonator.swift
@@ -11,7 +11,7 @@ import ShelfPlayerKit
 import SPPlayback
 
 internal final class IntentDonator {
-    var lastDonatedItem: Item?
+    internal var lastDonatedItem: Item?
     
     init() {
         lastDonatedItem = nil
