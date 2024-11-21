0bc28a9454af072f70b371b1bc4f16c5a7d5ce48
Rasmus Krämer <git@rfk.io>
2024-11-21 16:32:40 +0100

    Remove debug code

--- ShelfPlayerKit/Sources/SPOfflineExtended/Filesystem/DownloadManager+Handler.swift
+++ ShelfPlayerKit/Sources/SPOfflineExtended/Filesystem/DownloadManager+Handler.swift
@@ -63,8 +63,6 @@ extension DownloadManager: URLSessionDelegate, URLSessionDownloadDelegate {
             logger.fault("Failed to save context after download finished: \(error.localizedDescription)")
         }
         
-        print("a", OfflineManager.shared.offlineStatus(parentId: track.parentId))
-        
         NotificationCenter.default.post(name: PlayableItem.downloadStatusUpdatedNotification, object: track.parentId)
         logger.info("Download track finished: \(track.id)")
     }
