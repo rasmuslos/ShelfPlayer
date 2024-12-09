541cd7d4497cad53151671c439ed34540bc4d0a7
Rasmus Krämer <git@rfk.io>
2024-12-09 11:41:03 +0100

    Remeber last CarPlay library (#171)

--- Multiplatform/CarPlay/CarPlayTabBar.swift
+++ Multiplatform/CarPlay/CarPlayTabBar.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CarPlay
+import Defaults
 import ShelfPlayerKit
 
 internal final class CarPlayTabBar {
@@ -17,7 +18,10 @@ internal final class CarPlayTabBar {
     private var updateTask: Task<Void, Never>?
     private var libraries: [Library: LibraryTemplate]
     
+    private let delegate: CarPlayTabBarDelegate
     internal let template: CPTabBarTemplate
+    
+    private var didSelectLastActiveLibrary: Bool
     
     init(interfaceController: CPInterfaceController) {
         self.interfaceController = interfaceController
@@ -27,9 +31,19 @@ internal final class CarPlayTabBar {
         updateTask = nil
         libraries = [:]
         
+        delegate = .init()
         template = .init(templates: [offlineController.template])
         
+        didSelectLastActiveLibrary = false
+        
+        delegate.didSelect = didSelect
+        template.delegate = delegate
+        
         updateTemplates()
+    }
+    
+    var truncateLibraries: Bool {
+        libraries.count + 1 > CPTabBarTemplate.maximumTabCount
     }
     
     protocol LibraryTemplate {
@@ -38,6 +52,18 @@ internal final class CarPlayTabBar {
 }
 
 private extension CarPlayTabBar {
+    final class CarPlayTabBarDelegate: NSObject, CPTabBarTemplateDelegate {
+        var didSelect: ((Library) -> Void)! = nil
+        
+        func tabBarTemplate(_ tabBarTemplate: CPTabBarTemplate, didSelect selectedTemplate: CPTemplate) {
+            guard let userInfo = selectedTemplate.userInfo as? [String: Any], let library = userInfo["library"] as? Library else {
+                return
+            }
+            
+            didSelect(library)
+        }
+    }
+    
     var librariesListTemplate: CPListTemplate {
         let items = libraries.map {
             let library = $0.key
@@ -49,7 +75,9 @@ private extension CarPlayTabBar {
             
             item.handler = { _, completion in
                 Task {
-                    try await self.interfaceController.pushTemplate(template, animated: true)
+                    self.didSelect(library: library)
+                    
+                    // try await self.interfaceController.pushTemplate(template, animated: true)
                     completion()
                 }
             }
@@ -85,6 +113,9 @@ private extension CarPlayTabBar {
                 }
                 
                 controller.template.tabTitle = library.name
+                controller.template.userInfo = [
+                    "library": library
+                ]
                 
                 self.libraries[library] = controller
                 templates.append(controller.template)
@@ -94,11 +125,39 @@ private extension CarPlayTabBar {
                 return
             }
             
-            if templates.count > CPTabBarTemplate.maximumTabCount {
-                self.template.updateTemplates([self.offlineController.template, self.librariesListTemplate])
+            let lastActiveTemplate = self.libraries.first(where: { $0.key.id == Defaults[.lastCarPlayTabValue] })?.value.template
+            
+            if self.truncateLibraries {
+                var templates = [self.offlineController.template, self.librariesListTemplate]
+                
+                if let lastActiveTemplate {
+                    templates.append(lastActiveTemplate)
+                }
+                
+                self.template.updateTemplates(templates)
             } else {
                 self.template.updateTemplates(templates)
             }
+            
+            if self.truncateLibraries, let lastActiveTemplate {
+                self.template.select(lastActiveTemplate)
+            }
+            if !self.didSelectLastActiveLibrary, let template = self.libraries.first(where: { $0.key.id == Defaults[.lastCarPlayTabValue] })?.value.template {
+                self.template.select(template)
+                self.didSelectLastActiveLibrary = true
+            }
+        }
+    }
+    
+    func didSelect(library: Library) {
+        guard library.id != Defaults[.lastCarPlayTabValue] else {
+            return
+        }
+        
+        Defaults[.lastCarPlayTabValue] = library.id
+        
+        if self.truncateLibraries {
+            updateTemplates()
         }
     }
 }
