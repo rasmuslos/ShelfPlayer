bcd624642894db015c43b3b07fa08d021b7c4ec8
Rasmus Krämer <git@rfk.io>
2025-05-09 14:51:25 +0200

    Begin ConvenienceDownloads

---
+++ Multiplatform/Preferences/ConvenienceDownloadPreferences.swift
@@ -0,0 +1,31 @@
+//
+//  ConvenienceDownloadPreferences.swift
+//  Multiplatform
+//
+//  Created by Rasmus Krämer on 09.05.25.
+//
+
+import SwiftUI
+import Defaults
+import ShelfPlayerKit
+
+struct ConvenienceDownloadPreferences: View {
+    @Default(.enableListenNowDownloads) private var enableConvenienceDownloads
+    @Default(.enableConvenienceDownloads) private var enableListenNowDownloads
+    
+    var body: some View {
+        List {
+            Toggle("preferences.convenienceDownload.enable", isOn: $enableConvenienceDownloads)
+            Toggle("preferences.convenienceDownload.enableListenNowDownloads", isOn: $enableListenNowDownloads)
+            
+            Section("preferences.convenienceDownload.configurations") {
+                
+            }
+        }
+        .navigationTitle("preferences.convenienceDownload")
+    }
+}
+
+#Preview {
+    ConvenienceDownloadPreferences()
+}
