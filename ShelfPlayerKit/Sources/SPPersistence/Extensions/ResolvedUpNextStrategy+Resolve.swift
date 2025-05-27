071a94a0bc179455d312a5a4f82561f44ea5c95c
Rasmus Krämer <git@rfk.io>
2025-05-27 11:30:11 +0200

    Better resolving

---
+++ ShelfPlayerKit/Sources/SPPersistence/Extensions/ResolvedUpNextStrategy+Resolve.swift
@@ -0,0 +1,49 @@
+//
+//  ResolvedUpNextStrategy+Resolve.swift
+//  ShelfPlayerKit
+//
+//  Created by Rasmus Krämer on 27.05.25.
+//
+
+import Foundation
+import Defaults
+import SPFoundation
+
+public extension ResolvedUpNextStrategy {
+    func resolve(cutoff itemID: ItemIdentifier?) async throws -> [PlayableItem] {
+        switch self {
+            case .series(let seriesID):
+                let audiobooks = try await ABSClient[seriesID.connectionID].audiobooks(filtered: seriesID, sortOrder: nil, ascending: nil, limit: nil, page: nil).0
+                
+                guard let itemID else {
+                    return audiobooks
+                }
+                
+                guard let index = audiobooks.firstIndex(where: { $0.id == itemID }) else {
+                    throw ResolveError.missingCutoff
+                }
+                
+                return Array(audiobooks[(index + 1)...])
+            case .podcast(let podcastID):
+                let (_, episodes) = try await podcastID.resolvedComplex
+                
+                return await Podcast.filterSort(episodes,
+                                                filter: Defaults[.episodesFilter(podcastID)],
+                                                seasonFilter: Defaults[.episodesSeasonFilter(podcastID)],
+                                                restrictToPersisted: Defaults[.episodesRestrictToPersisted(podcastID)],
+                                                search: nil,
+                                                sortOrder: Defaults[.episodesSortOrder(podcastID)],
+                                                ascending: Defaults[.episodesAscending(podcastID)]).filter { $0.id != itemID }
+            case .listenNow:
+                return await ShelfPlayerKit.listenNowItems.filter { $0.id != itemID }
+            default:
+                throw ResolveError.invalidItemType
+        }
+        
+    }
+}
+
+private enum ResolveError: Error {
+    case missingCutoff
+    case invalidItemType
+}
