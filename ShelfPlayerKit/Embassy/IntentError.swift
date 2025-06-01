c490db79d6c47dc7a1c2a90337d7c70571dcd956
Rasmus Krämer <git@rfk.io>
2025-06-01 23:19:21 +0200

    Donate intents

--- ShelfPlayerKit/Embassy/IntentError.swift
+++ ShelfPlayerKit/Embassy/IntentError.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 public enum IntentError: Error, CustomLocalizedStringResourceConvertible {
+    case notFound
+    
     case noPlaybackItem
+    case invalidItemType
+    
     case wrongExecutionContext
     
     public var localizedStringResource: LocalizedStringResource {
         switch self {
+            case .notFound:
+                "intent.error.notFound"
+                
             case .noPlaybackItem:
                 "intent.error.noPlaybackItem"
+            case .invalidItemType:
+                "intent.error.invalidItemType"
             default:
                 "intent.error"
         }
