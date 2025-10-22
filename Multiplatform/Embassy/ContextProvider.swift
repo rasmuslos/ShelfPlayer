6c0dd633f27c21ffd712d7fe4a5811f746fbbc7b
Rasmus Krämer <git@rfk.io>
2025-10-22 20:40:19 +0200

    Improve library selection

--- Multiplatform/Embassy/ContextProvider.swift
+++ Multiplatform/Embassy/ContextProvider.swift
@@ -15,22 +15,23 @@ public struct ContextProvider {
     
     public static func updateUserContext() async {
         let libraries = await ShelfPlayerKit.libraries
-        let totalCount = await withTaskGroup {
-            for library in libraries {
-                $0.addTask {
-                    switch library.type {
-                    case .audiobooks:
-                        try? await ABSClient[library.connectionID].audiobooks(from: library.id, filter: .all, sortOrder: .added, ascending: false, limit: 1, page: 0).1
-                    case .podcasts:
-                        try? await ABSClient[library.connectionID].podcasts(from: library.id, sortOrder: .addedAt, ascending: false, limit: 1, page: 0).1
-                    }
-                }
-            }
-            
-            return await $0.reduce(0) {
-                $0 + ($1 ?? 0)
-            }
-        }
+        let totalCount = libraries.count * 42
+//        let totalCount = await withTaskGroup {
+//            for library in libraries {
+//                $0.addTask {
+//                    switch library.type {
+//                    case .audiobooks:
+//                        try? await ABSClient[library.connectionID].audiobooks(from: library.id, filter: .all, sortOrder: .added, ascending: false, limit: 1, page: 0).1
+//                    case .podcasts:
+//                        try? await ABSClient[library.connectionID].podcasts(from: library.id, sortOrder: .addedAt, ascending: false, limit: 1, page: 0).1
+//                    }
+//                }
+//            }
+//            
+//            return await $0.reduce(0) {
+//                $0 + ($1 ?? 0)
+//            }
+//        }
         
         let context = INMediaUserContext()
         context.numberOfLibraryItems = totalCount
