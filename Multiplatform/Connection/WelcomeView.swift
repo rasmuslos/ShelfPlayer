0a439ba1e0f3dce179393fda3f58885806b19fc8
Rasmus Krämer <git@rfk.io>
2026-03-05 08:00:45 +0100

    Add debug preferences to the welcome view

--- Multiplatform/Connection/WelcomeView.swift
+++ Multiplatform/Connection/WelcomeView.swift
@@ -12,53 +12,62 @@ struct WelcomeView: View {
     @Environment(Satellite.self) private var satellite
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            VStack(spacing: 0) {
-                Image(decorative: "Logo")
-                    .resizable()
-                    .aspectRatio(1, contentMode: .fit)
-                    .frame(width: 108)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                    .padding(.bottom, 28)
+        NavigationStack {
+            VStack {
+                Spacer()
                 
-                Text("setup.welcome")
-                    .bold()
-                    .font(.title)
-                    .fontDesign(.serif)
-            }
-            
-            Spacer()
-            
-            VStack(spacing: 2) {
-                HStack(spacing: 2) {
-                    Link(destination: URL(string: "https://github.com/rasmuslos/ShelfPlayer/blob/main/LICENSE")!) {
-                        Text(verbatim: "License")
-                    }
-                    Text(verbatim: " | ")
-                    Link(destination: URL(string: "https://github.com/rasmuslos/ShelfPlayer/blob/main/Privacy.md")!) {
-                        Text(verbatim: "Privacy Policy")
-                    }
+                VStack(spacing: 0) {
+                    Image(decorative: "Logo")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .frame(width: 108)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .padding(.bottom, 28)
+                    
+                    Text("setup.welcome")
+                        .bold()
+                        .font(.title)
+                        .fontDesign(.serif)
                 }
                 
-                Link(destination: URL(string: "https://github.com/rasmuslos/ShelfPlayer/blob/main/ToS.md")!) {
-                    Text(verbatim: "By using ShelfPlayer you agree to the ")
-                        .foregroundStyle(.primary)
-                    + Text(verbatim: "Terms of Service")
-                        .foregroundStyle(Color.accentColor)
+                Spacer()
+                
+                VStack(spacing: 2) {
+                    HStack(spacing: 2) {
+                        Link(destination: URL(string: "https://github.com/rasmuslos/ShelfPlayer/blob/main/LICENSE")!) {
+                            Text(verbatim: "License")
+                        }
+                        Text(verbatim: " | ")
+                        Link(destination: URL(string: "https://github.com/rasmuslos/ShelfPlayer/blob/main/Privacy.md")!) {
+                            Text(verbatim: "Privacy Policy")
+                        }
+                    }
+                    
+                    Link(destination: URL(string: "https://github.com/rasmuslos/ShelfPlayer/blob/main/ToS.md")!) {
+                        Text(verbatim: "By using ShelfPlayer you agree to the ")
+                            .foregroundStyle(.primary)
+                        + Text(verbatim: "Terms of Service")
+                            .foregroundStyle(Color.accentColor)
+                    }
+                    .buttonStyle(.plain)
                 }
-                .buttonStyle(.plain)
+                .font(.caption2)
+                .padding(.bottom, 4)
+                
+                Button("setup.welcome.action") {
+                    satellite.present(.addConnection)
+                }
+                .controlSize(.large)
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom, 8)
             }
-            .font(.caption2)
-            .padding(.bottom, 4)
-            
-            Button("setup.welcome.action") {
-                satellite.present(.addConnection)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("preferences.support", systemImage: "lifepreserver") {
+                        satellite.present(.debugPreferences)
+                    }
+                }
             }
-            .controlSize(.large)
-            .buttonStyle(.borderedProminent)
-            .padding(.bottom, 8)
         }
     }
 }
