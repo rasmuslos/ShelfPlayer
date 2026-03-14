65933a88262f62183f6ff0392446f2425abfdfe7
Rasmus Krämer <git@rfk.io>
2026-03-14 19:32:35 +0100

    Show progress in % in compact row

--- Multiplatform/Item/StatusOverlay.swift
+++ Multiplatform/Item/StatusOverlay.swift
@@ -62,7 +62,7 @@ struct StatusOverlay: View {
                                                 }
                                         } else if progress < 1 {
                                             Group {
-                                                if itemImageStatusPercentageText {
+                                                if itemImageStatusPercentageText || true {
                                                     Text(verbatim: "\(Int(progress * 100))")
                                                         .font(.system(size: fontSize))
                                                         .fontWeight(.heavy)
